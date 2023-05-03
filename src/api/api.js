332d12e3d832f761070388ffb0c64c7c8c0c23b0
oleksiiherasymchuk <aleks21@gmail.com>
2023-05-03 15:43:06 +0300

    Sign in modal + Redux-form, API.js

---
+++ src/api/api.js
@@ -0,0 +1,80 @@
+import axios from "axios";
+
+const instance = axios.create({
+  withCredentials: true,
+  baseURL: "https://social-network.samuraijs.com/api/1.0/",
+  header: {
+    "API-KEY": "367bb339-2821-49bb-868c-efaf25db8ec3",
+  },
+});
+
+export const authAPI = {
+  me() {
+    return instance.get(`auth/me`);
+  },
+  login(email, password, rememberMe = false, captcha = null) {
+    return instance.post(`auth/login`, {
+      email,
+      password,
+      rememberMe,
+      captcha,
+    });
+  },
+  logout() {
+    return instance.delete(`auth/logout`);
+  },
+};
+
+export const securityAPI = {
+  getCaptchaUrl() {
+    return instance.get(`security/get-captcha-url`);
+  },
+};
+
+export const profileAPI = {
+  getProfile(userId) {
+    return instance.get(`profile/${userId}`);
+    // return instance.get(`profile/` + userId);
+  },
+  getStatus(userId) {
+    return instance.get(`profile/status/${userId}`);
+  },
+  updateStatus(status) {
+    return instance.put(`profile/status`, { status: status });
+  },
+  savePhoto(photoFile) {
+    const formData = new FormData();
+    formData.append("image", photoFile);
+    return instance.put(`profile/photo`, formData, {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    });
+  },
+  saveProfile(profile){
+    return instance.put(`profile`, profile)
+  }
+};
+
+export const usersAPI = {
+  getProfile(userId) {
+    // console.warn("Obsolete method. Please profileAPI object.")
+    return profileAPI.getProfile(userId)
+  },
+  getUsers(currentPage = 1, pageSize = 10, term = "", friend = null) {
+    return instance
+      .get(
+        `users?page=${currentPage}&count=${pageSize}&term=${term}` +
+          (friend === null ? "" : `$friend=${friend}`)
+      )
+      .then((response) => {
+        return response.data;
+      });
+  },
+  follow(userId) {
+    return instance.post(`follow/${userId}`);
+  },
+  unfollow(userId) {
+    return instance.delete(`follow/${userId}`);
+  },
+};
--- src/components/Header/SignInModal/SignInModal.jsx
+++ src/components/Header/SignInModal/SignInModal.jsx
@@ -1,7 +1,93 @@
-import React, { useState } from "react";
-import { Button, Modal } from "antd";
+// validators on fields (errors and validate)
+// icons to fields 
 
-const SignInModal = () => {
+import React, { useState } from "react";
+import {
+  Button,
+  Modal,
+  // Input, Tooltip, Checkbox
+} from "antd";
+import s from "./SignInModal.module.css";
+// import {
+//   InfoCircleOutlined,
+//   UserOutlined,
+//   SecurityScanOutlined,
+// } from "@ant-design/icons";
+import { reduxForm, Field } from "redux-form";
+import { connect } from "react-redux";
+import { login } from "../../../redux/authReducer";
+
+// const InputEmail = () => {
+//   return (
+//     <>
+//       <Input
+//         placeholder="Enter your username"
+//         name="email"
+//         prefix={<UserOutlined className="site-form-item-icon" />}
+//         suffix={
+//           <Tooltip title="Extra information">
+//             <InfoCircleOutlined
+//               style={{
+//                 color: "rgba(0,0,0,.45)",
+//               }}
+//             />
+//           </Tooltip>
+//         }
+//       />
+//     </>
+//   );
+// };
+
+// const InputPassword = () => {
+//   return (
+//     <Input.Password
+//       placeholder="Enter your password"
+//       name="password"
+//       prefix={<SecurityScanOutlined className="site-form-item-icon" />}
+//     />
+//   );
+// };
+
+// const InputCheckbox = () => {
+//   return <Checkbox name="checkbox">Remember me</Checkbox>;
+// };
+
+const LoginForm = ({ handleSubmit }) => {
+  return (
+    <div className={s.loginForm}>
+      <form onSubmit={handleSubmit}>
+          <Field
+            className={s.formInput}
+            name="email"
+            component="input"
+            placeholder="Enter your email"
+            type="email"
+          />
+          <Field
+            className={s.formInput}
+            name="password"
+            component="input"
+            placeholder="Enter your password"
+            type="password"
+          />
+          <Field
+            className={s.formInputCheckbox}
+            name="rememberMe"
+            component="input"
+            type="checkbox"
+          />{" "}
+          Remember me
+          <Button type="primary" block>
+            <button>log in</button>
+          </Button>
+      </form>
+    </div>
+  );
+};
+
+const LoginReduxForm = reduxForm({ form: "login" })(LoginForm);
+
+const SignInModal = (props) => {
   const [isModalOpen, setIsModalOpen] = useState(false);
 
   const showModal = () => {
@@ -16,23 +102,42 @@ const SignInModal = () => {
     setIsModalOpen(false);
   };
 
+  const onSubmit = (formData) => {
+    props.login(formData.email, formData.password, formData.rememberMe)
+  };
+
+  if(props.isAuth) {
+    // withAuthRedirect HOC
+    // add withRouter
+    // return <Redirect to={"/news"}/>
+  }
+
+
   return (
     <>
-      <Button type="primary" onClick={showModal} style={{height:'40px', borderRadius:'10px', fontSize:'16px'}}>
+      <Button
+        type="primary"
+        onClick={showModal}
+        style={{ height: "40px", borderRadius: "10px", fontSize: "16px" }}
+      >
         Sign In
       </Button>
       <Modal
-        title="Basic Modal"
+        title="Login Modal"
         open={isModalOpen}
         onOk={handleOk}
         onCancel={handleCancel}
       >
-        <p>Some contents...</p>
-        <p>Some contents...</p>
-        <p>Some contents...</p>
+        <LoginReduxForm onSubmit={onSubmit} captchaUrl={props.captchaUrl} />
       </Modal>
     </>
   );
 };
 
-export default SignInModal;
+const mapStateToProps = (state) => ({
+  captchaUrl: state.auth.captchaUrl,
+  isAuth: state.auth.isAuth,
+});
+
+export default connect(mapStateToProps, { login })(SignInModal);
+
